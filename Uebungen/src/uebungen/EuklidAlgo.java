09c453dac61576411730716f4862e6ddc6b5a86c
Blackduster <Achim.Winter@yahoo.de>
2016-10-12 00:23:02 +0200

    Euklidscher Algorithmus hinzugefügt

---
+++ Uebungen/src/uebungen/EuklidAlgo.java
@@ -0,0 +1,25 @@
+package uebungen;
+import java.util.Scanner;
+
+public class EuklidAlgo {
+
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b , r;
+		sc = new Scanner(System.in);
+		System.out.print("Bitte eine Zahl A eingeben: ");
+		a = sc.nextInt();
+		System.out.print("Bitte eine Zahl B eingeben: ");
+		b = sc.nextInt();
+		while(b != 0){
+			r = a % b;
+			a = b;
+			b =r;
+			
+		}
+		System.out.println("Der ggT lautet " + a);
+	}
+
+}
