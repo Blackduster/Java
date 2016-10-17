f5be35d7e507665dd3bc7dc65f172236be18be42
awinter <Achim.Winter@yahoo.de>
2016-10-17 13:54:40 +0200

    Aufgabe2 und 3 hinzugefuegt

---
+++ Uebungen/src/uebungen/Aufgabe1.java
@@ -0,0 +1,10 @@
+package uebungen;
+
+public class Aufgabe1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Willkommen zu Programmieren1");
+	}
+
+}
---
+++ Uebungen/src/uebungen/Aufgabe3.java
@@ -0,0 +1,11 @@
+package uebungen;
+
+public class Aufgabe3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3*(9+3)+4*8;
+		System.out.println(x);
+	}
+
+}
