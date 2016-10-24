2ac58459b87d2aa941711f5f370cece61e265fb7
awinter <achim.winter@yahoo.de>
2016-10-24 13:16:06 +0200

    Ueb2_Aufgabe4 hinzugefügt

--- Uebungen/src/uebungen/Uebungsblatt2/Ueb2_Aufgabe4.java
+++ Uebungen/src/uebungen/Uebungsblatt2/Ueb2_Aufgabe4.java
@@ -1,10 +1,43 @@
 package uebungen.Uebungsblatt2;
 
+import java.util.Date;
+
 /**
  * Created by awinter on 24.10.16.
  */
 public class Ueb2_Aufgabe4 {
 
+    public static void main(String[] args){
 
+        Date date = new Date();
+        int stunden = date.getHours();
+        int minuten = date.getMinutes();
+        int sekunden = date.getSeconds();
+
+        //a
+        int seknow = (stunden * 60 * 60) + (minuten *60) + sekunden;
+        System.out.println("Sekunden seit Mitternacht: " + seknow);
+
+        //b
+        int maxsek = 24 * 60 * 60;
+        System.out.println("Verbleibende Sekunden: " + (maxsek - seknow));
+
+        //c
+        System.out.println("Prozent vergangen: " + ((seknow * 100.0) / maxsek ));
+
+        /*
+        SimpleDateFormat std = new SimpleDateFormat("HH");
+        String StrsStunden = std.format(new Date());
+        int stunden = Integer.parseInt(StrsStunden);
+
+        Date
+        //a
+        System.out.println("Sekunden seit Mitternacht: " + stunden*60*60);
+        //b
+        SimpleDateFormat all = new SimpleDateFormat("HH.mm.ss");
+        System.out.print(all);
+        */
+
+    }
 
 }
