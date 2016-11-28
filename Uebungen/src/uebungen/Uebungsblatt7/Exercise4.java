97c1efe3d0787be07c5d2d7e42bca72263f4e80c
Blackduster <Achim.Winter@yahoo.de>
2016-11-28 20:06:11 +0100

    Eigene Methode für mehrfache verwendete for Schleife erstellt

--- Uebungen/src/uebungen/Uebungsblatt7/Exercise4.java
+++ Uebungen/src/uebungen/Uebungsblatt7/Exercise4.java
@@ -3,19 +3,19 @@ package uebungen.Uebungsblatt7;
 import java.util.Scanner;
 
 public class Exercise4 {
-    public static void main() {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
 
-        for (int i = input / 2; i > 0; i = i - 2) {
-            System.out.print(" ");
-        }
-        for (int j = 1; j <= input; j++) {
-            System.out.print("*");
-        }
-        for (int k = input / 2; k > 0; k = k - 2) {
-            System.out.print(" ");
+        for (int i = 1; i <= input; i++) {
+
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println("");
         }
 
     }
