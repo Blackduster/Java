f5e6198a9ec85088ac57246813761b3f1c7b446b
awinter <achim.winter@yahoo.de>
2016-12-05 14:48:57 +0100

    Uebungsblatt8 hinzugefügt

---
+++ Uebungen/src/uebungen/Uebungsblatt8/Exercise1.java
@@ -0,0 +1,29 @@
+package uebungen.Uebungsblatt8;
+import java.util.Scanner;
+
+/**
+ * Created by awinter on 05.12.16.
+ */
+public class Exercise1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Bitte geben Sie die Basis ein: ");
+        int base = sc.nextInt();
+        System.out.print("Bitte geben Sie den Exponenten ein: ");
+        int exp = sc.nextInt();
+
+        System.out.println(recexp(base, exp));
+
+    }
+
+    private static int recexp(int base, int exp){
+        if (exp == 0){
+            return 1;
+        }
+        else {
+            return base * recexp(base, (exp - 1));
+        }
+
+    }
+
+}
---
+++ Uebungen/src/uebungen/Uebungsblatt8/Exercise2.java
@@ -0,0 +1,55 @@
+package uebungen.Uebungsblatt8;
+import java.util.Scanner;
+
+/**
+ * Created by awinter on 05.12.16.
+ */
+public class Exercise2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= 0){
+            System.out.println("Ungültigen Wert eingegeben");
+            System.exit(0);
+        }
+        double[] Werte = new double[n];
+        for (int i = 0; i < n; i++){
+            System.out.print((i +1) + ". Wert: ");
+            Werte[i] = sc.nextDouble();
+        }
+        System.out.println("Kleinster Wert: " + getMinValue(Werte));
+        System.out.println("Größter Wert: " + getMaxValue(Werte));
+        System.out.println("Arithmetischer Mittelwert: " + getAverage(Werte));
+    }
+    private static double getMinValue(double[] Werte){
+        double minvalue = Werte[0];
+        for (int i = 0; i <= Werte.length; i++){
+            if (i == Werte.length)
+                return minvalue;
+            if(minvalue > Werte[i]){
+                minvalue = Werte[i];
+            }
+        }
+        return minvalue;
+    }
+
+    private static double getMaxValue(double[] Werte){
+        double maxvalue = Werte[0];
+        for (int i = 0; i <= Werte.length; i++){
+            if (i == Werte.length)
+                return maxvalue;
+            if(maxvalue < Werte[i]){
+                maxvalue = Werte[i];
+            }
+        }
+        return maxvalue;
+    }
+    private static double getAverage(double[] Werte){
+        double average = Werte[0];
+        for (int i = 1; i < Werte.length; i++) {
+            average += Werte[i];
+        }
+        return (average / Werte.length);
+    }
+
+}
