610d74993b296f8d1861c67db5b0a4294ce56c74
awinter <achim.winter@yahoo.de>
2016-12-19 12:30:09 +0100

    Aufgabe aus Aufgabenblatt 9

--- Uebungen/src/uebungen/Uebungsblatt9/Exercise4.java
+++ Uebungen/src/uebungen/Uebungsblatt9/Exercise4.java
@@ -1,23 +1,43 @@
 package uebungen.Uebungsblatt9;
-
 import java.util.Scanner;
 
 public class Exercise4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        System.out.print(pot(2, 8));
-        System.out.print(5);
-        for (int i = 0; i < input; i++) {
-            int a = 2;
+        double input = sc.nextDouble();
+        System.out.println(sin(input));
+    }
+
+    private static double sin(double x){
+        double summand = 0;
+        double summe = 0;
+        int vorzeichen = 1;
+        double zaehler = 0;
+        double nenner = 0;
+        for (int i=3; i < 20; i = i+2){
+            zaehler = vorzeichen * pot(x, i);
+            nenner = fac(i);
+            summand = zaehler / nenner;
+            summe = summe + summand;
+            vorzeichen *= -1;
+        }
+        return x-summe;
+    }
+
+    private static double pot(double base, int pot) {
+        if(pot==0){
+            return 1;
+        }else{
+            return base * pot(base, pot-1);
         }
     }
 
-    private static int pot(int base, int pot) {
-        if (pot == 0) {
+    private static long fac(int zahl){
+        if(zahl == 0) {
             return 1;
-        } else {
-            return base *= base;
+        }else{
+            return zahl * fac(zahl-1);
         }
     }
+
 }
